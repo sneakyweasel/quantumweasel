5e51e1549a0c17c6a531ec4060d430c6e8454a94
Philippe Cochin <philippe.cochin@gmail.com>
2019-09-05 11:49:34 +0800

    Updated readme and fixed sec vulns

--- main.ts
+++ main.ts
@@ -1,27 +1,29 @@
-import * as math from 'mathjs'
-import Cluster from './Cluster.js'
-import Coord from './Coord.js'
-import Grid from './Grid.js'
-import Cell from './Cell.js'
+// import * as math from 'mathjs'
+import Coord from './Coord'
+import Element from './Element'
+import Cell from './Cell'
+// import Cluster from './Cluster'
+import Grid from './Grid'
 
 // INIT GRID
-const grid = new Grid(8, 8)
+const grid = new Grid(8, 8, [])
 
 // LOAD COORD
 const coord = new Coord(6, 6)
-const cell = new Cell(coord, 9)
+const cell = new Cell(coord, Element.blank(), 0, false)
 cell.display()
 grid.set(cell)
 
-// CREATE ROW VECTOR
-const row = []
-const rangeX = [1, 4]
-for (let x = rangeX[0]; x < rangeX[1]; x++) {
-    row.push(new Cell(new Coord(2, x), math.randomInt(1, 9)))
-}
-const cluster = new Cluster(row)
-grid.addCluster(cluster)
 grid.display()
+// CREATE ROW VECTOR
+// const row = []
+// const rangeX = [1, 4]
+// for (let x = rangeX[0]; x < rangeX[1]; x++) {
+//     row.push(new Cell(new Coord(2, x), math.randomInt(1, 9)))
+// }
+// const cluster = new Cluster(row)
+// grid.addCluster(cluster)
+// grid.display()
 
 // VECTOR FROM NUMBER ARRAY
 // const newvec = Vector.fromArray(0, 1, [3, 3, 3], true)
@@ -41,10 +43,10 @@ grid.display()
 // console.log(`Index ${index} correspond to X: ${coord2.x} - Y: ${coord2.y}`)
 
 // SUBMATRIX SELECTION
-const cellA = new Coord(2, 6)
-const cellB = new Coord(3, 5)
-const selection = grid.submatrix(cellA, cellB)
+// const cellA = new Coord(2, 6)
+// const cellB = new Coord(3, 5)
+// const selection = grid.submatrix(cellA, cellB)
 // selection.forEach((elem) => {
 //     console.log(elem.toArray())
 // })
-selection.toString()
+// selection.toString()
