34c960a91acb7424b2308857a9d142fca6355daf
Philippe Cochin <philippe.cochin@gmail.com>
2019-10-10 22:36:23 +0800

    Interferometer design level

--- src/Coord.ts
+++ src/Coord.ts
@@ -3,92 +3,92 @@
 // Coord is a [x, y, z?] convenient way to deal with coordinates.
 
 export interface CoordInterface {
-	x: number;
-	y: number;
+  x: number;
+  y: number;
 }
 
 export default class Coord {
-	x: number;
-	y: number;
+  x: number;
+  y: number;
 
-	constructor(y: number, x: number) {
-		this.y = y;
-		this.x = x;
-	}
+  constructor(y: number, x: number) {
+    this.y = y;
+    this.x = x;
+  }
 
-	// Conversion: coord -> uid
-	id(rows: number): number {
-		return this.y * rows + this.x;
-	}
+  // Conversion: coord -> uid
+  id(rows: number): number {
+    return this.y * rows + this.x;
+  }
 
-	// SVG coordinate system: top-left point of cell
-	pos(spacing: number): [number, number] {
-		const y = this.y * spacing;
-		const x = this.x * spacing;
-		return [y, x];
-	}
+  // SVG coordinate system: top-left point of cell
+  pos(spacing: number): [number, number] {
+    const y = this.y * spacing;
+    const x = this.x * spacing;
+    return [y, x];
+  }
 
-	// Adjacent cells
-	get top(): Coord {
-		return Coord.importCoord({ y: this.y - 1, x: this.x });
-	}
-	get bottom(): Coord {
-		return Coord.importCoord({ y: this.y + 1, x: this.x });
-	}
-	get left(): Coord {
-		return Coord.importCoord({ y: this.y, x: this.x - 1 });
-	}
-	get right(): Coord {
-		return Coord.importCoord({ y: this.y, x: this.x + 1 });
-	}
-	get adjacent(): Coord[] {
-		return [this.top, this.right, this.bottom, this.left];
-	}
-	get array(): number[] {
-		return [this.y, this.x];
-	}
+  // Adjacent cells
+  get top(): Coord {
+    return Coord.importCoord({ y: this.y - 1, x: this.x });
+  }
+  get bottom(): Coord {
+    return Coord.importCoord({ y: this.y + 1, x: this.x });
+  }
+  get left(): Coord {
+    return Coord.importCoord({ y: this.y, x: this.x - 1 });
+  }
+  get right(): Coord {
+    return Coord.importCoord({ y: this.y, x: this.x + 1 });
+  }
+  get adjacent(): Coord[] {
+    return [this.top, this.right, this.bottom, this.left];
+  }
+  get array(): number[] {
+    return [this.y, this.x];
+  }
 
-	// Check if two coordinates are adjacent
-	isAdjacent(coord: Coord): boolean {
-		return coord.isIncludedIn(this.adjacent);
-	}
+  // Check if two coordinates are adjacent
+  isAdjacent(coord: Coord): boolean {
+    return coord.isIncludedIn(this.adjacent);
+  }
 
-	// Check for equality
-	equal(coord: Coord): boolean {
-		return this.x === coord.x && this.y === coord.y;
-	}
+  // Check for equality
+  equal(coord: Coord): boolean {
+    return this.x === coord.x && this.y === coord.y;
+  }
 
-	// Test inclusion in array of coords
-	isIncludedIn(coords: Coord[]): boolean {
-		return (
-			coords.filter(coord => {
-				return this.equal(coord);
-			}).length > 0
-		);
-	}
+  // Test inclusion in array of coords
+  isIncludedIn(coords: Coord[]): boolean {
+    return (
+      coords.filter(coord => {
+        return this.equal(coord);
+      }).length > 0
+    );
+  }
 
-	// override of toString method for debugging
-	toString(): string {
-		return `[Y:${this.y}, X:${this.x}]`;
-	}
+  // override of toString method for debugging
+  toString(): string {
+    return `[Y:${this.y}, X:${this.x}]`;
+  }
 
-	// Export JSON
-	exportCoord(): CoordInterface {
-		return {
-			y: this.y,
-			x: this.x
-		};
-	}
+  // Export JSON
+  exportCoord(): CoordInterface {
+    return {
+      y: this.y,
+      x: this.x
+    };
+  }
 
-	// Export JSON
-	static importCoord(json: CoordInterface): Coord {
-		return new Coord(json.y, json.x);
-	}
+  // Export JSON
+  static importCoord(json: CoordInterface): Coord {
+    return new Coord(json.y, json.x);
+  }
 
-	// Conversion: uid -> coord
-	static fromId(index: number, cols: number): Coord {
-		const x = index % cols;
-		const y = Math.floor(index / cols);
-		return Coord.importCoord({ y: y, x: x });
-	}
+  // Conversion: uid -> coord
+  static fromId(index: number, cols: number): Coord {
+    const x = index % cols;
+    const y = Math.floor(index / cols);
+    return Coord.importCoord({ y: y, x: x });
+  }
 }
