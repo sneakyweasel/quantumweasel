9a1bc4f554febbbaa1d5c8ca3458f17353f27661
Philippe Cochin <philippe.cochin@gmail.com>
2019-10-15 18:49:21 +0800

    Added webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    entry: "./src/app.ts",
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            loader: "ts-loader",
-            exclude: /node_modules/
-        }]
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"]
-    },
-    output: {
-        filename: "app.js",
-        path: path.resolve(__dirname, 'dist')
-    },
-    devtool: "inline-source-map",
-    mode: "development"
+  entry: "./src/app.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "QuantumWeasel",
+    libraryTarget: "var"
+  },
+  devtool: "inline-source-map",
+  mode: "development"
 };
