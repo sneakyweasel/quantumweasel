322ea69500f1beef37d2ba885f7d3cef8ac31138
Philippe Cochin <philippe.cochin@gmail.com>
2019-10-17 16:16:57 +0800

    Added Corner Cube

--- data/elements.ts
+++ data/elements.ts
@@ -1,11 +1,11 @@
 import { ElementInterface } from "./../src/Element";
-// import { Elem } from "../src/Helpers";
+import { Elem } from "../src/Helpers";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const jsonElements: ElementInterface[] = [
   {
-    // name: Elem.Laser,
-    name: "Laser",
+    // name: "Laser",
+    name: Elem.Laser,
     group: "Source",
     description: "An on-demand single photon source.",
     active: true,
@@ -16,8 +16,8 @@ export const jsonElements: ElementInterface[] = [
     tiles: [[0, 0], [0, 1], [0, 2], [0, 3]]
   },
   {
-    // name: Elem.Absorber,
-    name: "Absorber",
+    // name: "Rock",
+    name: Elem.Rock,
     group: "Absorption",
     description:
       "Dark and immersive as your sweetheart's depth of eyes. Absorbs light. And is sensitive.",
@@ -29,8 +29,8 @@ export const jsonElements: ElementInterface[] = [
     tiles: [[1, 0]]
   },
   {
-    // name: Elem.PolarizerH,
-    name: "PolarizerH",
+    // name: "PolarizerH",
+    name: Elem.PolarizerH,
     group: "Polarization",
     description:
       "A polarization filter WE...Anisotropic polymer strands capture electric oscillations parallel to them. Used in photography.",
@@ -42,8 +42,21 @@ export const jsonElements: ElementInterface[] = [
     tiles: [[2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7]]
   },
   {
-    // name: Elem.QuarterWavePlateH,
-    name: "QuarterWavePlateH",
+    // name: "PolarizerH",
+    name: Elem.PolarizerV,
+    group: "Polarization",
+    description:
+      "A polarization filter NS...Anisotropic polymer strands capture electric oscillations parallel to them. Used in photography.",
+    active: false,
+    absorption: 0,
+    phase: 0,
+    id: 2,
+    ascii: ["🡢", "🡥", "🡡", "🡤", "🡠", "🡧", "🡣", "🡦"],
+    tiles: [[2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7]]
+  },
+  {
+    // name: "QuarterWavePlateH",
+    name: Elem.QuarterWavePlateH,
     group: "Polarization",
     description:
       "It delays one polarization (with darker lines) by λ/4. When applied correctly, it can change linear polarization into circular, and vice versa.",
@@ -55,8 +68,21 @@ export const jsonElements: ElementInterface[] = [
     tiles: [[3, 0], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7]]
   },
   {
-    // name: Elem.Detector,
-    name: "Detector",
+    // name: "QuarterWavePlateH",
+    name: Elem.QuarterWavePlateV,
+    group: "Polarization",
+    description:
+      "It delays one polarization (with darker lines) by λ/4. When applied correctly, it can change linear polarization into circular, and vice versa.",
+    active: false,
+    absorption: 0,
+    phase: 0,
+    id: 3,
+    ascii: ["🡪", "🡭", "🡩", "🡬", "🡨", "🡯", "🡫", "🡮"],
+    tiles: [[3, 0], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7]]
+  },
+  {
+    // name: "Detector",
+    name: Elem.Detector,
     group: "Absorption",
     description:
       "Detects and amplifies electric signal from each single photon, from a single direction. Your goal is to get photon there!",
@@ -68,8 +94,8 @@ export const jsonElements: ElementInterface[] = [
     tiles: [[4, 0], [4, 1], [4, 2], [4, 3]]
   },
   {
-    // name: Elem.DetectorFour,
-    name: "DetectorFour",
+    // name: "DetectorFour",
+    name: Elem.DetectorFour,
     group: "Absorption",
     description:
       "Detects and amplifies electric signal from each single photon, from all directions. Typically, it is the goal to get the photon here.",
@@ -81,8 +107,8 @@ export const jsonElements: ElementInterface[] = [
     tiles: [[7, 0], [7, 1], [7, 2], [7, 3]]
   },
   {
-    // name: Elem.SugarSolution,
-    name: "SugarSolution",
+    // name: "SugarSolution",
+    name: Elem.SugarSolution,
     group: "Polarization",
     description:
       "Table sugar is a chiral molecule – it does not look the same as its mirror reflection. We put it in an amount, so it rotates polarization by 45°.",
@@ -94,8 +120,8 @@ export const jsonElements: ElementInterface[] = [
     tiles: [[8, 0]]
   },
   {
-    // name: Elem.CoatedBeamSplitter,
-    name: "CoatedBeamSplitter",
+    // name: "CoatedBeamSplitter",
+    name: Elem.CoatedBeamSplitter,
     group: "Direction",
     description:
       "A thin slab of glass with a reflective layer - reflecting half the beam and transmitting the other half of it.",
@@ -107,8 +133,8 @@ export const jsonElements: ElementInterface[] = [
     tiles: [[9, 0], [9, 1], [9, 2], [9, 3], [9, 4], [9, 5], [9, 6], [9, 7]]
   },
   {
-    // name: Elem.Mine,
-    name: "Mine",
+    // name: "Mine",
+    name: Elem.Mine,
     group: "Absorption",
     description:
       "Once it absorbs a single photon, it sets off. Old french submarine captains can sometimes disarm them.",
@@ -120,8 +146,8 @@ export const jsonElements: ElementInterface[] = [
     tiles: [[11, 0]]
   },
   {
-    // name: Elem.PolarizingBeamSplitter,
-    name: "PolarizingBeamSplitter",
+    // name: "PolarizingBeamSplitter",
+    name: Elem.PolarizingBeamSplitter,
     group: "Direction",
     description:
       "Reflects vertical polarization (↕), transmitts horizonal polarization (↔).",
@@ -133,8 +159,20 @@ export const jsonElements: ElementInterface[] = [
     tiles: [[12, 0], [12, 1]]
   },
   {
-    // name: Elem.Mirror,
-    name: "Mirror",
+    // name: "CornerCube",
+    name: Elem.CornerCube,
+    group: "Direction",
+    description: "Reflects any incoming photon.",
+    active: false,
+    absorption: 0,
+    phase: 0,
+    id: 13,
+    ascii: ["*"],
+    tiles: [[13, 0]]
+  },
+  {
+    // name: "Mirror",
+    name: Elem.Mirror,
     group: "Direction",
     description: "Metallic or dielectric mirror.",
     active: false,
@@ -154,8 +192,8 @@ export const jsonElements: ElementInterface[] = [
     ]
   },
   {
-    // name: Elem.BeamSplitter,
-    name: "BeamSplitter",
+    // name: "BeamSplitter",
+    name: Elem.BeamSplitter,
     group: "Direction",
     description:
       "A thin slab of glass reflecting half the beam, and transmitting other half of it.",
@@ -176,8 +214,8 @@ export const jsonElements: ElementInterface[] = [
     ]
   },
   {
-    // name: Elem.Glass,
-    name: "Glass",
+    // name: "Glass",
+    name: Elem.Glass,
     group: "Phase",
     description:
       "Higher refractive index makes light slower. We set its thickness so it retards the phase by λ/4. Useful for changing interference.",
@@ -189,8 +227,8 @@ export const jsonElements: ElementInterface[] = [
     tiles: [[16, 0]]
   },
   {
-    // name: Elem.VacuumJar,
-    name: "VacuumJar",
+    // name: "VacuumJar",
+    name: Elem.VacuumJar,
     group: "Phase",
     description:
       "Even air retards light a bit. We set the thickness of vacuum so it advances the phase by λ/4. Useful for changing interference.",
@@ -202,8 +240,8 @@ export const jsonElements: ElementInterface[] = [
     tiles: [[17, 0]]
   },
   {
-    // name: Elem.Absorber,
-    name: "Absorber",
+    // name: "Absorber",
+    name: Elem.Absorber,
     group: "Absorption",
     description: "Filter with 50% absorption probability.",
     active: false,
@@ -214,8 +252,8 @@ export const jsonElements: ElementInterface[] = [
     tiles: [[18, 0]]
   },
   {
-    // name: Elem.Void,
-    name: "Void",
+    // name: "Void",
+    name: Elem.Void,
     group: "Basic",
     description: "The void...",
     active: false,
@@ -226,8 +264,8 @@ export const jsonElements: ElementInterface[] = [
     tiles: [[19, 0]]
   },
   {
-    // name: Elem.Wall,
-    name: "Wall",
+    // name: "Wall",
+    name: Elem.Wall,
     group: "Basic",
     description: "A standard wall.",
     active: false,
@@ -238,8 +276,8 @@ export const jsonElements: ElementInterface[] = [
     tiles: [[20, 0], [20, 1]]
   },
   {
-    // name: Elem.Gate,
-    name: "Gate",
+    // name: "Gate",
+    name: Elem.Gate,
     group: "Basic",
     description: "A gate that opens when an adjacent detector is active.",
     active: false,
@@ -248,10 +286,11 @@ export const jsonElements: ElementInterface[] = [
     id: 21,
     ascii: ["W", "M"],
     tiles: [[21, 0], [21, 1]]
+    // tiles: [[22, 0], [22, 1]]
   },
   {
-    // name: Elem.FaradayRotator,
-    name: "FaradayRotator",
+    // name: "FaradayRotator",
+    name: Elem.FaradayRotator,
     group: "Polarization",
     description:
       "Rotates polarization with magnetic field by 45°. Has different symmetries than Sugar Solution. A building block for optical diodes.",
