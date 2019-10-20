d3825515a027a92e04196a48165a8da88234d075
Philippe Cochin <philippe.cochin@gmail.com>
2019-10-20 14:49:27 +0800

    Added levels

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 // import json from "../levels/dev/interferometer.json";
 // import json from "../levels/dev/beamsplitter.json";
-// import json from "../levels/classic/level19.json";
-import json from "../levels/classic/level26.json";
+import json from "../levels/classic/level31.json";
 import Level from "./Level";
 import Game from "./Game";
 
